57aa27a82f9bc2c37e1829c8efcf03f54a74c109
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-26 15:22:30 +0100

    Add Users

--- users/src/App.js
+++ users/src/App.js
@@ -1,27 +1,34 @@
 import React from "react";
-
-import "./App.css";
-
-import users from "./users.json";
-
 import UserInfo from "./components/UserInfo";
-
-const data = users.map((user) => {
-  return (
-    <UserInfo name={user.name} email={user.email} website={user.website} />
-  );
-});
+import Users from "./components/users.json";
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
-      <div>
-        <div>
-          <ul>{data}</ul>
-        </div>
+      <div className="App">
+
+        <h1>Users list</h1>
+
+        {/*  affichez la liste des utilisateurs (nom, email, website) */}
+        {/* {Users.map(user => (
+          <p key={user.id}> Name: {user.name} Email: {user.email} Website: {user.website}</p> 
+        ))} */}
+        
+        {/* UserInfo */}
+        {Users.map(user => ( 
+          <UserInfo 
+            user={user.id}
+            name={user.name} 
+            email={user.email} 
+            website={user.website} 
+          />
+        ))}
+        
+        
       </div>
     );
-  }
+  }  
+  
 }
 
-export default App;
+export default App
